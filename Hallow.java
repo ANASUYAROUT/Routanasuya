a02da792e50942f0a9de610c6822db0bc1e42b24
Anasuya Rout <routanasuya896@gmail.com>
2025-07-27 01:40:23 +0530

    Add files via upload

---
+++ Hallow.java
@@ -0,0 +1,17 @@
+public class Hallow {
+ public static void main(String[] args) {
+    int n = 4;
+    int m = 5;
+    for(int i=1; i<=n; i++) {
+for(int j=1; j<=m; j++) {
+    // call(i , j)common
+if(i==1 || j==1 || i==n || i==m) {
+    System.out.print("*");
+} else {
+    System.out.println(" ");
+}
+System.err.println();
+}
+    }
+ }   
+}
