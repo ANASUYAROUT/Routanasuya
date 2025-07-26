df469099abe0962018afac7ee4d686fd0b41a525
Anasuya Rout <routanasuya896@gmail.com>
2025-07-27 01:20:43 +0530

    Add files via upload

---
+++ Button.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Button {
+  public static void main (String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int button = sc.nextInt();
+    if(button == 1) {
+        System.out.println("LAlAt");
+    } else if(button == 2) {
+        System.out.println("LOVE");
+    } else if(button == 3) {
+        System.out.println("ANASUYA");
+    } else {
+      System.out.println("Invalid button");  
+    }
+  } 
+}
