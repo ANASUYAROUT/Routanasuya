610fdee25f9964d622eb751aa36a2043dc36a0ad
Anasuya Rout <routanasuya896@gmail.com>
2025-07-28 20:43:50 +0530

    Add files via upload

---
+++ ArrayMarks.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ArrayMarks {
+    public static void main(String[] args) {
+    int[]marks = new int[3];
+ marks[0] = 56;
+ marks[1] = 78;
+ marks[2] = 55;
+
+for(int i=0; i<3; i++) {
+    System.out.println(marks[i]);
+}
+
+   
+ 
+ 
+
+    }
+}
